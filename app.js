7a604c3a6757950ba30da55e4c4e59c70563240a
thekinita <thekinita@mail.ru>
2022-12-08 04:37:30 +0500

    Mobile complete
    Довёл до ума мобильную версию svg, но desktop версия поломалась

--- app.js
+++ app.js
@@ -74,18 +74,27 @@ $(function() {
         pauseOnHover: false,
     });
 
+    $('[slider-forMap]').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      arrows: false,
+      fade: false,
+      asNavFor: '[map-slider]',
+    });
+
     $('[map-slider]').slick({
         infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        fade: true,
+        fade: false,
         arrows: true,
         dots: false,
         adaptiveHeight: true,
         mobileFirst: true,
         draggable: false,
         swipe: false,
-
+        asNavFor: '[slider-forMap]',
     });
 
     $('[slider-for]').slick({
@@ -94,7 +103,7 @@ $(function() {
       adaptiveHeight: true,
       arrows: false,
       fade: true,
-      asNavFor: '[region-slider]'
+      asNavFor: '[region-slider]',
     });
 
     $('[region-slider]').slick({
@@ -108,7 +117,7 @@ $(function() {
         mobileFirst: true,
         prevArrow: '<button type="button" class="slick-arrow_region slick-prev">Previous</button>',
         nextArrow: '<button type="button" class="slick-arrow_region slick-next">Next</button>',
-        asNavFor: '[slider-for]'
+        asNavFor: '[slider-for]',
     });
 
 });
